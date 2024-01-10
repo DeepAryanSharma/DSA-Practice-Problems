bc113e96dbac2ee520650a4990c3ceaa97aa1231
Deep Aryan Sharma <124386531+DeepAryanSharma@users.noreply.github.com>
2024-01-11 00:06:03 +0530

    Create LowestCommonAncestor.cpp

---
+++ LowestCommonAncestor.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //base case
+        if(root==NULL){
+            return NULL;
+        }
+        
+        //check current node for p & q
+        if(root->val == p->val){
+            return p;
+        }
+        if(root->val ==q->val){
+            return q;
+        }
+
+        //else p and q can be in left or right
+        TreeNode* leftAns=lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightAns=lowestCommonAncestor(root->right, p, q);
+
+        if(leftAns==NULL && rightAns==NULL){
+            return NULL;
+        }
+        else if(leftAns !=NULL && rightAns==NULL){
+            return leftAns;
+        }
+        else if(leftAns==NULL && rightAns!=NULL){
+            return rightAns;
+        }
+        else{
+            return root;
+        }
+    }
+};
